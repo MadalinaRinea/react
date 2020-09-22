8a36c1d1365a5adc6ed5d05f5cda7d1db5ebe5e4
MadalinaRinea <66742630+MadalinaRinea@users.noreply.github.com>
2020-09-22 18:20:50 +0300

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import Counter from './Counter/Counter';
+// import React from 'react';
+// import Counter from './Counter/Counter';
 
-//JSX
+// //JSX
+// function App() {
+//     // return <h1>Hello from App component own modules!</h1>;
+//     return (
+//         <div>
+//             <h1>Counter</h1>
+//             <Counter />
+//         </div>
+//     );
+// }
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Nav from './Nav';
+import GamesList from '../features/Games/GamesList';
+import GameDetails from '../features/Games/GameDetails';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 function App() {
-    // return <h1>Hello from App component own modules!</h1>;
     return (
-        <div>
-            <h1>Counter</h1>
-            <Counter />
+        <div className="container">
+            <Router>
+                <Nav />
+
+                <Route exact path="/" component={ () => <h1>Homepage</h1> } />
+                <Route exact path="/games" component={ GamesList } />
+                <Route exact path="/games/:id" component={ GameDetails } />
+            </Router>
         </div>
     );
 }
 
 export default App;
-
